dd39660e7cac925b4e1a5604f5ab2c7ce97c5574
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2021-01-17 18:54:05 -0300

    tratando exceções

--- 1. JUnit/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ 1. JUnit/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -2,13 +2,15 @@ package br.ce.wcaquino.servicos;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -16,26 +18,80 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
 	
+	@Rule
+	public ExpectedException exception= ExpectedException.none();
+
 	@Test
-	public void teste() {
-		//cenário
+	public void testeLocacao() throws Exception {
+		// cenário
 		LocacaoService service = new LocacaoService();
 		Usuario usuarioTeste = new Usuario("João");
 		Filme filmeAlugado = new Filme("Orgulho e Preconceito", 5, 10.25);
-		
-		//ação
-		Locacao aluguel = service.alugarFilme(usuarioTeste, filmeAlugado);	
-		
-		//verificação
+
+		// ação
+		Locacao aluguel = service.alugarFilme(usuarioTeste, filmeAlugado);
+
+		// verificação
 		Assert.assertTrue(DataUtils.isMesmaData(aluguel.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(aluguel.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		Assert.assertEquals(10.25, aluguel.getValor(), 0.01); //valor esperado, valor real, margem erro
-	
-		//verificação com AssertThat
-		assertThat(aluguel.getValor(), is(10.25)); //com import estatico
+		Assert.assertEquals(10.25, aluguel.getValor(), 0.01); // valor esperado, valor real, margem erro
+
+		// verificação com AssertThat
+		assertThat(aluguel.getValor(), is(10.25)); // com import estatico
 		assertThat(aluguel.getValor(), is(CoreMatchers.not(5.0)));
 		assertThat(DataUtils.isMesmaData(aluguel.getDataLocacao(), new Date()), is(true));
-		assertThat(DataUtils.isMesmaData(aluguel.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));		
+		assertThat(DataUtils.isMesmaData(aluguel.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+
+		// utilizando @Rules
+		error.checkThat(aluguel.getValor(), is(10.25));
+		error.checkThat(aluguel.getValor(), is(CoreMatchers.not(5.0)));
+	}
+
+	@Test(expected = Exception.class)
+	public void testeLocacao_filmeSemEstoque() throws Exception {
+		// cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuarioTeste = new Usuario("João");
+		Filme filmeAlugado = new Filme("Orgulho e Preconceito", 0, 10.25);
+
+		// ação
+		Locacao aluguel = service.alugarFilme(usuarioTeste, filmeAlugado);
+	}
+	
+	@Test
+	public void testeLocacao_filmeSemEstoque_2(){
+		
+		// cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuarioTeste = new Usuario("João");
+		Filme filmeAlugado = new Filme("Orgulho e Preconceito", 0, 10.25);
+		Locacao aluguel;
+				
+		// ação
+		try {
+			aluguel = service.alugarFilme(usuarioTeste, filmeAlugado);	
+			Assert.fail("Deveria ter lançado uma exceção");
+		} catch (Exception e) {
+			Assert.assertThat(e.getMessage(), is("Filme sem estoque"));
+		}
+	}
+	
+	@Test
+	public void testeLocacao_filmeSemEstoque_3() throws Exception{
+		
+		// cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuarioTeste = new Usuario("João");
+		Filme filmeAlugado = new Filme("Orgulho e Preconceito", 0, 10.25);
+		
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+						
+		// ação
+		service.alugarFilme(usuarioTeste, filmeAlugado);	
 	}
 }
